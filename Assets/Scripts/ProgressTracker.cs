c1fbbacb7c74aeab93bb1a749548520b3b25cc64
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2022-02-24 22:18:54 +0000

    Progress Tracker Script

---
+++ Assets/Scripts/ProgressTracker.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressTracker : MonoBehaviour
+{
+    [SerializeField] private GameObject[] photos;
+    [SerializeField] private GameObject[] voidPhotos;
+    [SerializeField] private static bool[] photosUnlock;
+    void Update()
+    {
+        if (photosUnlock[1])
+        {
+            photos[1].SetActive(true);
+            voidPhotos[0].SetActive(false);
+        }
+        else
+        {
+            photos[1].SetActive(false);
+            voidPhotos[0].SetActive(true);
+        }
+
+        if (photosUnlock[2])
+        {
+            photos[2].SetActive(true);
+            voidPhotos[1].SetActive(false);
+        }
+        else
+        {
+            photos[2].SetActive(false);
+            voidPhotos[1].SetActive(true);
+        }
+    }
+}
