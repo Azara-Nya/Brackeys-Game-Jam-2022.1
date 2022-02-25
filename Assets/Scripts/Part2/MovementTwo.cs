bbb3f60b76e881787e6b171181a5dfb6f5cfae34
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2022-02-25 20:04:40 +0000

    Restart Ability

--- Assets/Scripts/Part2/MovementTwo.cs
+++ Assets/Scripts/Part2/MovementTwo.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MovementTwo : MonoBehaviour
 {
@@ -13,5 +14,11 @@ public class MovementTwo : MonoBehaviour
         Movement.y = Input.GetAxisRaw("Vertical");
 
         rb.MovePosition(rb.position + Movement * moveSpeed * Time.fixedDeltaTime);
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name); ;
+        }
     }
+
 }
