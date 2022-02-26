368f9f0c99a3fa48cb5c162d5f3d80e9909f37c5
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2022-02-26 19:54:49 +0000

    Updated 3rd Movement Script
    Animations & Flipping

--- Assets/Scripts/Flatty.cs
+++ Assets/Scripts/Flatty.cs
@@ -7,10 +7,37 @@ public class Flatty : MonoBehaviour
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 10f;
     Vector2 Movement;
+    private float moveInput;
+    [SerializeField] private Animator animator;
+    private bool facingRight = true;
     void Update()
     {
+        float moveInput = Input.GetAxis("Horizontal");
+        if (facingRight == false && moveInput > 0)
+        {
+            flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            flip();
+        }
+        if (moveInput == 0)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+        else
+        {
+            animator.SetBool("IsRunning", true);
+        }
         Movement.x = Input.GetAxisRaw("Horizontal");
 
         rb.MovePosition(rb.position + Movement * moveSpeed * Time.fixedDeltaTime);
+    }
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
     }
 }
